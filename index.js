12c9207675adfce589ede6af96a94653f1bf2cea
el-sam <samurai.h@yahoo.com>
2023-10-14 16:27:19 +0800

    passing 1st test. but need further research on unit ' feet'

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function scuberGreetingForFeet(){
   // Write your code here!
+  let height = 350;
+  let msg;
+  if (height <= 400) {
+    msg = "This one is on me!";
+    return msg;
+  }
 }
 
 function ternaryCheckCity(){
